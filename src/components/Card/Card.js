dcfa4dee02031a59419dbc9c16b86c1d5e453e93
Faiz Murtadha <faizmrtdha@gmail.com>
2022-06-27 00:29:32 +0700

    change card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,40 @@
-const Card = ({ name, ability, hp }) => {
+import { Link } from 'react-router-dom';
+
+const Card = ({ data }) => {
+  const { name, sprites, base_experience } = data;
   return (
-    <div className="max-w-max">
-      <div className="flex flex-col p-6 border border-solid border-blue-400">
-        <div className="object-cover object-center">
-          <img
-            src="https://picsum.photos/200"
-            alt="Pokemon img"
-            className="w-full object-cover object-center"
-          />
-        </div>
-        <div className="flex flex-wrap">
-          <p className="w-full">Name :</p>
-          <p className="w-full">Name :</p>
-          <p className="w-full">Name :</p>
-          <p className="w-full">Name :</p>
-        </div>
+    <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <Link to="/">
+        <img class="rounded-t-lg w-full" src={sprites.front_shiny} alt="" />
+      </Link>
+      <div class="p-5">
+        <Link to="/">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
+            {name.toUpperCase()}
+          </h5>
+        </Link>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          Here are the biggest enterprise technology acquisitions of 2021 so
+          far, in reverse chronological order.
+        </p>
+        <Link
+          to="/"
+          class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Read more
+          <svg
+            class="ml-2 -mr-1 w-4 h-4"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </Link>
       </div>
     </div>
   );
