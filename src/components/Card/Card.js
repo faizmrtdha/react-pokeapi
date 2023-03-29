61e4d041a3fb0904a945d3ddf757cc9d8b349513
Faiz Murtadha <faizmrtdha99@gmail.com>
2023-03-29 08:52:42 +0700

    Adjust card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 
 const Card = ({ data }) => {
-  const { name, sprites, base_experience } = data;
+  const { name, sprites, base_experience, stats } = data;
   return (
     <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
       <Link to="/">
@@ -9,14 +9,19 @@ const Card = ({ data }) => {
       </Link>
       <div class="p-5">
         <Link to="/">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
-            {name.toUpperCase()}
+          <h5 class="mb-2 uppercase text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
+            {name}
           </h5>
         </Link>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </p>
+        <div className="flex flex-wrap w-full gap-x-4">
+          {stats.map(({ stat, base_stat }) => {
+            return (
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 uppercase">
+                {stat.name} : <span>{base_stat}</span>
+              </p>
+            );
+          })}
+        </div>
         <Link
           to="/"
           class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
